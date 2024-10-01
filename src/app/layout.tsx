cd98e8ad509f4de3bbf8c01ec194cb5af1f58f05
JamieNguyen <adungg2509@gmail.com>
2024-10-02 00:26:15 +0700

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google'
+
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "Currency Converter App",
+  description: "Currency Converter App",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
